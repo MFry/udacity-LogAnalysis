ff84b91330c268f3b16d24a8d7a92506dc189cb1
Michal Frystacky <Michal.Frystacky@alaska.gov>
2017-09-19 18:09:53 -0800

    Feat: Temporary tables for author&article pop
    Temporary table for:
    Authors popularity
    Article popularity

--- vagrant/analysis/logAnalyzerForums.py
+++ vagrant/analysis/logAnalyzerForums.py
@@ -43,24 +43,50 @@ def set_url_popularity(cur):
                 ''')
 
 
+def set_article_popularity(cur):
+    cur.execute('''
+                CREATE TEMPORARY TABLE article_hits AS (
+                  SELECT
+                    title,
+                    total,
+                    articles.author AS author_id
+                  FROM url_hits
+                  LEFT JOIN articles
+                  ON POSITION(articles.slug IN url_hits.path) > 0
+                )
+                ''')
+
+
+def set_authors_popularity(cur):
+    cur.execute('''
+                CREATE TEMPORARY TABLE authors_popularity AS (
+                  SELECT
+                    name,
+                    total
+                  FROM article_hits
+                  LEFT JOIN authors
+                  ON author_id = authors.id
+                )
+                ''')
+
+
 def getMostPopularArticles(cur, top=3):
-        cur.execute('''
-                     SELECT *
-                     FROM url_hits
-                     LIMIT %s;
-                    ''', (top, ))
-        return cur.fetchall()
+    cur.execute('''
+                 SELECT *
+                 FROM article_hits
+                 LIMIT %s;
+                ''', (top, ))
+    return cur.fetchall()
 
 
-def listAllAuthorsPopularity():
-    with get_cursor() as cur:
-        cur.execute('''
-                    SELECT 
-                       *
-                    FROM
-                      url_hits
-                     INNER JOIN articles ON 
-                    ''')
+def listAllAuthorsPopularity(cur, top=3):
+    cur.execute('''
+                SELECT 
+                   *
+                FROM authors_popularity
+                LIMIT %s
+                ''', (top,))
+    return cur.fetchall()
 
 
 if __name__ == '__main__':
@@ -69,4 +95,7 @@ if __name__ == '__main__':
         data = cur.fetchall()
         pprint(data)
         set_url_popularity(cur)
+        set_article_popularity(cur)
+        set_authors_popularity(cur)
         pprint(getMostPopularArticles(cur))
+        pprint(listAllAuthorsPopularity(cur))
